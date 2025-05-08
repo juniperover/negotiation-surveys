905ee176ac347bec58a5ce3ebb29ade605874b85
Jen Overbeck <j.overbeck@mbs.edu>
2025-05-08 12:42:44 +1000

    move javascript completely out to svo.js, update for formatting

--- old_unused_files/svo2_deprecated.js
+++ old_unused_files/svo2_deprecated.js
@@ -1,31 +1,3 @@
-// Define SVO values for each item at each position
-const svoValues = {
-  1: {
-    self: [85, 85, 85, 85, 85, 85, 85, 85, 85],
-    other: [85, 76, 68, 59, 50, 41, 33, 24, 15]
-  },
-  2: {
-    self: [85, 87, 89, 91, 93, 94, 96, 98, 100],
-    other: [15, 19, 24, 28, 33, 37, 41, 46, 50]
-  },
-  3: {
-    self: [50, 54, 59, 63, 68, 72, 76, 81, 85],
-    other: [100, 98, 96, 94, 93, 91, 89, 87, 85]
-  },
-  4: {
-    self: [50, 54, 59, 63, 68, 72, 76, 81, 85],
-    other: [100, 89, 79, 68, 58, 47, 36, 26, 15]
-  },
-  5: {
-    self: [100, 94, 88, 81, 75, 69, 63, 56, 50],
-    other: [50, 56, 63, 69, 75, 81, 88, 94, 100]
-  },
-  6: {
-    self: [100, 98, 96, 94, 93, 91, 89, 87, 85],
-    other: [50, 54, 59, 63, 68, 72, 76, 81, 85]
-  }
-};
-
 // Function to update the displayed values and hidden inputs
 function updateSVOValues(item, value) {
   const index = value - 1; // Convert to 0-based index
@@ -112,16 +84,3 @@ function updateSVOValues(item, value) {
   // Add the table to the values container
   valuesContainer.appendChild(table);
 }
-
-// Initialize all sliders when the document is ready
-$(document).ready(function() {
-  for (let i = 1; i <= 6; i++) {
-    // Initialize with default value
-    updateSVOValues(i, 5);
-    
-    // Add change listener
-    $('#svo' + i).on('change', function(e) {
-      updateSVOValues(i, parseInt(e.target.value));
-    });
-  }
-});
